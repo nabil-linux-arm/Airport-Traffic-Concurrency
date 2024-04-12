e57bb47fe5875223e62f15884ab9d548ccb287a5
Nabil Mazwardi <a1833491@student.adelaide.edu.au>
2024-04-12 17:37:57 +0930

    Added Runway declarations

---
+++ src/Runway.cpp
@@ -0,0 +1,6 @@
+#include "Runway.hpp"
+
+Runway::Runway(){}
+
+void Runway::simulate(){}
+void Runway::addAirplaneToQueue(std::shared_ptr<Airplane> airplane){}
--- src/Runway.hpp
+++ src/Runway.hpp
@@ -9,13 +9,44 @@
 
 class Airplane;
 
-class Runway : public AirportObject
+class WaitingAirplanes
 {
     public:
-        // Behavior functions
-        void addAirplaneToQueue(std::shared_ptr<Airplane> airplane);
-                
+
+        int getSize();
+
+        void pushBack(std::shared_ptr<Airplane> airplane, std::promise<void> &&promise);
+        void permitEntry();
+
     private:
+
+        std::vector<std::shared_ptr<Airplane>> _airplanes;
+        std::vector<std::promise<void>> _promises;     
+        std::mutex _mtx;
+};
+
+class Runway : public AirportObject, public std::enable_shared_from_this<Runway>
+{
+    public:
+        
+        Runway();
+
+        // Behavior functions
+        void simulate();
+        void addAirplaneToQueue(std::shared_ptr<Airplane> airplane);
+
+        std::shared_ptr<Runway> getExitRunway() { return _exitRunway; }
+        void setExitRunway(std::shared_ptr<Runway> runway) { _exitRunway = runway; }
+        double getLength() { return _length; }
+        void setLength(double length) { _length = length; }
+
+        std::shared_ptr<Runway> get_shared_this() { return shared_from_this(); }
+
+    private:
+
+        double _length;                        // Length of the Runway
+        std::shared_ptr<Runway> _exitRunway;    // The runway the airplane exits into after entering this one
+        WaitingAirplanes _waitingQueue;
 
 };
 
