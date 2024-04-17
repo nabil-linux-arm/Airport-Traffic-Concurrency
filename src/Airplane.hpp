098c2922ff64d163f90f4a186fe943cc4423f01f
Nabil Mazwardi <a1833491@student.adelaide.edu.au>
2024-04-18 09:28:09 +0930

    Added functionality to add and remove airplane from runway

--- src/Airplane.hpp
+++ src/Airplane.hpp
@@ -15,6 +15,7 @@ class Airplane : public AirportObject, public std::enable_shared_from_this<Airpl
 
         // Behaviour functions
         void simulate();
+        void setCurrentRunway(std::shared_ptr<Runway> runway);
 
         std::shared_ptr<Airplane> get_shared_this() { return shared_from_this(); }
 
@@ -27,7 +28,7 @@ class Airplane : public AirportObject, public std::enable_shared_from_this<Airpl
         void startTimer(int min, int max);  // Starts the delay for planes to be flying
         
         std::shared_ptr<Port> _port;           // Port the Airplane is currently docked at (Undefined when not)
-        std::shared_ptr<Runway> _nextRunway;   // The runway that the Airplane is currently waiting to enter
+        std::shared_ptr<Runway> _currentRunway;   // The runway that the Airplane is currently waiting to enter
         double _posRunway;                     // Position of Airplane on runway
         double _speed;                         // Speed of Airplane on runway
 
--- src/Runway.cpp
+++ src/Runway.cpp
@@ -6,10 +6,27 @@ Runway::Runway(){}
 void Runway::simulate(){}
 void Runway::addAirplaneToQueue(std::shared_ptr<Airplane> airplane)
 {
-    printf("[Runway] - ADDING AIRPLANE %d", airplane->getID());
+    printf("[Runway] - ADDING AIRPLANE, id: %d\n", airplane->getID());
+    _waitingQueue.pushBack(airplane);
 }
 
-int WaitingAirplanes::getSize(){}
+int WaitingAirplanes::getSize()
+{
+    return _airplanes.size();
+}
 
-void WaitingAirplanes::pushBack(std::shared_ptr<Airplane> airplane, std::promise<void> &&promise){}
-void WaitingAirplanes::permitEntry(){}
+void WaitingAirplanes::pushBack(std::shared_ptr<Airplane> airplane)
+{
+    _airplanes.push_back(airplane);
+}
+
+void WaitingAirplanes::permitEntry()
+{
+    printf("[Runway] - REMOVING AIRPLANE, Current size of waiting Queue: %d\n", this->getSize());
+    printf("[Runway] - REMOVING AIRPLANE, id: %d\n", _airplanes.back()->getID());
+    _airplanes.pop_back();
+}
+void Runway::permitAirplaneIn()
+{
+    _waitingQueue.permitEntry();
+}
